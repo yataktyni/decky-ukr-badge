0809c2e176f213dc338c87b89793b3423c86a398
yataktyni <yataktyni@users.noreply.github.com>
2025-06-13 02:28:10 +0300

    chore: add @rollup/plugin-commonjs as a dev dependency and update pnpm-lock.yaml with its version

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,11 @@ import deckyPlugin from "@decky/rollup";
 
 export default deckyPlugin({
   external: ['react', 'react-dom'],
+  output: {
+    globals: {
+      react: 'SP_REACT',
+      'react-dom': 'SP_REACT',
+    },
+  },
   // Add your extra Rollup options here
 });
