2d904babec32c195d966ba29cc943b14678925fe
yataktyni <hungryrabbitjam@github.com>
2026-01-10 00:57:09 +0200

    Remove update-version script and drop image field

--- scripts/update-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the version from package.json
-VERSION=$(jq -r .version < package.json)
-
-# Update the version in plugin.json
-jq ".version = \"$VERSION\"" < plugin.json > plugin.json.tmp
-mv plugin.json.tmp plugin.json 
